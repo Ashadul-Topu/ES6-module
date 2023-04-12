5177060557b25856a64feffcf5d957c293cae3d2
Ashadul-Topu <ashaduldemo@gmail.com>
2023-04-12 09:37:53 +0600

    ES6 Module Import Export

---
+++ main.js
@@ -0,0 +1,5 @@
+import {demo, setText} from './myModule.js'
+console.log(demo);
+
+let newText = setText("Module is easy to learn")
+console.log(newText);
---
+++ myModule.js
@@ -0,0 +1,9 @@
+export {demo, setText}
+
+let demo = 'Hello this is module demo text';
+
+let text;
+function setText(txt) {
+     text = txt
+     return text
+}
